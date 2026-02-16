caf00b9a76f5c5d92b157bd254d67c4464ff1c8e
AmirMahdiAzad <amirmahdi.azad.2010@gmail.com>
2026-02-16 17:58:55 +0330

    add color for project

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,17 @@
 import SearchContact from "./contact/searchcontact";
-// test
+import { Background, PURPLE } from "../helpers/color";
+
 const Navbar = () => {
   return (
-    <nav className="bg-gray-900 text-white shadow-lg">
+    <nav
+      className="bg-gray-900 text-white shadow-lg"
+      style={{ backgroundColor: Background }}
+    >
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex flex-col sm:flex-row items-center justify-between gap-4 py-4">
           {/* Title */}
           <div className="text-lg sm:text-xl font-bold">
-            اپلیکیشن مدیریت <span className="text-purple-500">مخاطبین</span>
+            اپلیکیشن مدیریت <span style={{ color: PURPLE }}>مخاطبین</span>
           </div>
           <SearchContact />
           {/* Search Box */}
